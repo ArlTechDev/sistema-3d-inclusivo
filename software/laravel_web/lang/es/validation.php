22527645507f8f355e858efc06e15c96ed6b632e
Yometa999 <mi.paso.por.incos@gmail.com>
2026-09-04 15:38:45 -0400

    feat(web): incorporar mensajes de validacion amigables en espanol

---
+++ software/laravel_web/lang/es/validation.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'accepted' => 'El campo :attribute debe ser aceptado.',
+    'confirmed' => 'La confirmación de :attribute no coincide.',
+    'email' => 'El campo :attribute debe ser un correo electrónico válido.',
+    'max' => [
+        'numeric' => 'El campo :attribute no debe ser mayor a :max.',
+        'string' => 'El campo :attribute no debe tener más de :max caracteres.',
+    ],
+    'min' => [
+        'numeric' => 'El campo :attribute debe ser al menos :min.',
+        'string' => 'El campo :attribute debe tener al menos :min caracteres.',
+    ],
+    'numeric' => 'El campo :attribute debe ser un número.',
+    'required' => 'El campo :attribute es obligatorio.',
+    'unique' => 'El valor de :attribute ya se encuentra registrado en el sistema.',
+    'attributes' => [
+        'name' => 'nombre completo',
+        'email' => 'correo electrónico',
+        'password' => 'contraseña',
+        'cantidad' => 'cantidad de piezas',
+        'texto_personalizado' => 'texto en Braille',
+        'gramos_pla' => 'gramos de filamento PLA',
+    ],
+];
