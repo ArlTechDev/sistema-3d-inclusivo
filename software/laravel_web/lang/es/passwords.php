321c21b654df7d5523890cbffc5e440e817e4539
Yometa999 <mi.paso.por.incos@gmail.com>
2026-09-04 15:38:44 -0400

    feat(web): agregar traducciones para restablecimiento seguro de claves

---
+++ software/laravel_web/lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => '¡Tu contraseña ha sido restablecida exitosamente!',
+    'sent' => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor espera antes de volver a intentarlo.',
+    'token' => 'Este enlace de restablecimiento de contraseña no es válido.',
+    'user' => 'No encontramos ningún usuario con esa dirección de correo electrónico.',
+];
